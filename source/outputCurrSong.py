225e6f587a28a1ab565c7f3b608f609492eb6a1d
bentarman <btarman@mymail.mines.edu>
2017-09-26 00:25:23 -0600

    added next, previous, and replay buttons.

---
+++ source/outputCurrSong.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import sys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import memcache
+#sudo apt-get install memcached python-memcache
+import multiprocessing as mp
+import youtubeSongs as yt
+
+
+if __name__ == "__main__":
+    while True:
+        yt.initYoutube() #only executes once!
+
+        yt.displayCurrSong()
+
+        #uneccessary rn but may need to multiprocess for i3 implementatoin
+        nextSong = mp.Process(target = yt.nextSong)
+
+        userInput = input()
+        if (userInput == "n"):
+            nextSong.start()
+            nextSong.join()
+        elif (userInput== "p"):
+            yt.previousSong()
+        elif (userInput == "r"):
+            yt.replaySong()
+
+
+
+'''
+#driver = webdriver.Remote(command_executor=session_url,desired_capabilities={})
+#driver.session_id = session_id
+shared = memcache.Client(['127.0.0.1:11211'], debug=0)
+print(shared.get('SessionId'))
+
+session_id = shared.get('SessionId')
+session_url  = shared.get('SessionUrl')
+
+driver = webdriver.Remote(command_executor = session_url, desired_capabilities = {})
+driver.session_id = session_id
+
+
+
+
+songDisplayed = False
+currUrl = ""
+
+browser = settings.browser
+
+if songDisplayed and currUrl == browser.current_url:
+    pass
+
+else:
+    currUrl = browser.current_url
+    time.sleep(3)
+    elem = browser.find_element_by_class_name("style-scope ytd-video-primary-info-renderer")
+
+
+songName = ""
+for c in elem.text:
+    songName += c
+    if c == "\n":
+        break
+print(songName)
+songDisplayed = True
+'''
--- source/youtubeSongs.py
+++ source/youtubeSongs.py
@@ -2,61 +2,102 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import sys
+import memcache
 
-'''
 
-ffprofile = webdriver.FirefoxProfile()
-adblockfile = '/home/bentarman/Downloads/adblock_plus-2.9.1-an+fx+sm+tb.xpi'
-ffprofile.add_extension(adblockfile)
-ffprofile.set_preference("extensions.adblockplus.currentVersion", "2.4")
-browser = webdriver.Firefox(ffprofile)
-'''
+
 fp = webdriver.FirefoxProfile('/home/bentarman/.mozilla/firefox/rkvkm76d.default')
 
 browser = webdriver.Firefox(fp)
-#browser = webdriver.Firefox(executable_path = r'/usr/bin/geckodriver')
 browser.get("https://www.youtube.com/")
 
+def run_once(f):
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            wrapper.has_run = True
+            return f(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
 
-#sign in to googlePlus
+@run_once
+def initYoutube():
+    '''
 
-elem = browser.find_element_by_class_name("style-scope paper-button")
-
-elem.click()
-
-time.sleep(2)
-#elem = browser.find_element_by_id("identifierId")
-#elem.send_keys("email here", Keys.RETURN)
-
-time.sleep(1)
-
-elem = browser.find_element_by_xpath('//div[@id="password"]')
-elem.send_keys(sys.argv[1], Keys.RETURN)
-
-time.sleep(3)
-#search for music
-elem = browser.find_element_by_id("search-input")
-elem.send_keys("my mix", Keys.RETURN)
-
-time.sleep(3)
+    ffprofile = webdriver.FirefoxProfile()
+    adblockfile = '/home/bentarman/Downloads/adblock_plus-2.9.1-an+fx+sm+tb.xpi'
+    ffprofile.add_extension(adblockfile)
+    ffprofile.set_preference("extensions.adblockplus.currentVersion", "2.4")
+    browser = webdriver.Firefox(ffprofile)
+    '''
+    global browser
+    global fp
 
 
 
-elem = browser.find_element_by_id("thumbnail")
-elem.click()
+    #sign in to googlePlus
+    elem = browser.find_element_by_class_name("style-scope paper-button")
+    elem.click()
 
-songDisplayed = False
-currUrl = ""
+    time.sleep(2)
+    #elem = browser.find_element_by_id("identifierId")
+    #elem.send_keys("email here", Keys.RETURN)
 
-while True:
-    if songDisplayed and currUrl == browser.current_url:
+    time.sleep(1)
+
+    elem = browser.find_element_by_xpath('//div[@id="password"]')
+    elem.send_keys(sys.argv[1], Keys.RETURN)
+
+    time.sleep(3)
+    #search for music
+    elem = browser.find_element_by_id("search-input")
+    elem.send_keys("my mix", Keys.RETURN)
+    time.sleep(5)
+
+    elem = browser.find_element_by_class_name("style-scope ytd-thumbnail-overlay-hover-text-renderer")
+    time.sleep(2)
+    elem.click()
+
+
+
+'''
+    session_url = browser.command_executor._url
+    session_id = browser.session_id
+
+    print(session_id)
+    shared = memcache.Client(['127.0.0.1:11211'], debug=0)
+    shared.set('SessionId', session_id)
+    shared.set('SessionUrl', session_url)
+
+
+'''
+
+
+def nextSong():
+    global browser
+    elem = browser.find_element_by_xpath("//a[@Title='Next']")
+    elem.click()
+
+def previousSong():
+    global browser
+    elem = browser.find_element_by_xpath("//a[@Title='Replay']")
+    elem.click()
+    elem.click()
+
+def replaySong():
+    global browser
+    elem = browser.find_element_by_xpath("//a[@Title='Replay']")
+    elem.click()
+
+def displayCurrSong():
+    global browser
+    currUrl = ""
+
+    if currUrl == browser.current_url:
         pass
-
     else:
         currUrl = browser.current_url
-        time.sleep(3)
+        time.sleep(1.2)
         elem = browser.find_element_by_class_name("style-scope ytd-video-primary-info-renderer")
-
 
         songName = ""
         for c in elem.text:
@@ -64,4 +105,25 @@ while True:
             if c == "\n":
                 break
         print(songName)
-        songDisplayed = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
